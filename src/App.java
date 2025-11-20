f3248907adbe07d3339564e403c4d6a0a319901a
valuthringer <valentin.luthringer@outlook.fr>
2025-11-20 11:50:03 +0100

    td v

--- src/App.java
+++ src/App.java
@@ -93,121 +93,98 @@ public class App {
   public static void afficheAttributs(Class<?> cl, String decalage) {
     // Affichage des attributs de la classe : utilise afficheTypeBasic ou
     // afficheTypeFull
-    // Code à écrire
-    // Field[] fields = cl.getDeclaredFields();
-    // for (Field field : fields) {
-    // System.out.print(decalage + Modifier.toString(field.getModifiers()) + " ");
-    // afficheTypeBasic(field.getType(), decalage);
-    // System.out.println(" " + field.getName() + ";");
-    // }
+    Field[] fields = cl.getDeclaredFields();
+    for (Field field : fields) {
+      System.out.print(decalage + Modifier.toString(field.getModifiers()) + " ");
+      afficheTypeBasic(field.getType(), decalage);
+      System.out.println(" " + field.getName() + ";");
+    }
   }
 
   private static void afficheTypeBasic(Class<?> cl, String decalage) {
     // Affichage du type de l'attribut : Ne prend pas en compte l'instanciation de
-    // types génériques (variante simple)
-    // Code à écrire
-    // System.out.print(decalage + cl.getSimpleName());
-
+    // types génériques
+    System.out.print(cl.getSimpleName());
   }
 
   private static void afficheTypeFull(Class<?> t, Type gt, String decalage) {
     // Affichage du type de l'attribut : Prend en compte l'instanciation de types
-    // génériques (variante plus complexe)
-    // Code à écrire
-    // if (gt instanceof ParameterizedType) {
-    // ParameterizedType pType = (ParameterizedType) gt;
-    // System.out.print(decalage + ((Class<?>) pType.getRawType()).getSimpleName() +
-    // "<");
-    // Type[] typeArgs = pType.getActualTypeArguments();
-    // for (int i = 0; i < typeArgs.length; i++) {
-    // if (typeArgs[i] instanceof Class<?>) {
-    // System.out.print(((Class<?>) typeArgs[i]).getSimpleName());
-    // } else {
-    // System.out.print(typeArgs[i].getTypeName());
-    // }
-    // if (i < typeArgs.length - 1) {
-    // System.out.print(", ");
-    // }
-    // }
-    // System.out.print(">");
-    // } else if (t instanceof Class<?>) {
-    // System.out.print(decalage + ((Class<?>) t).getSimpleName());
-    // } else {
-    // System.out.print(decalage + t.getTypeName());
-    // }
-
+    // génériques
+    if (gt instanceof ParameterizedType) {
+      ParameterizedType pType = (ParameterizedType) gt;
+      System.out.print(((Class<?>) pType.getRawType()).getSimpleName() + "<");
+      Type[] typeArgs = pType.getActualTypeArguments();
+      for (int i = 0; i < typeArgs.length; i++) {
+        if (typeArgs[i] instanceof Class<?>) {
+          System.out.print(((Class<?>) typeArgs[i]).getSimpleName());
+        } else {
+          System.out.print(typeArgs[i].getTypeName());
+        }
+        if (i < typeArgs.length - 1) {
+          System.out.print(", ");
+        }
+      }
+      System.out.print(">");
+    } else {
+      System.out.print(t.getTypeName());
+    }
   }
 
   public static void afficheConstructeurs(Class<?> cl, String decalage) {
     // Affichage des constructeurs de la classe : utilise afficheParamsBasic ou
     // afficheParamsFull
-    // Code à écrire
-    // Constructor<?>[] constructors = cl.getDeclaredConstructors();
-    // for (Constructor<?> constructor : constructors) {
-    // System.out.print(decalage + Modifier.toString(constructor.getModifiers()) + "
-    // " + constructor.getName() + "(");
-    // Class<?>[] paramTypes = constructor.getParameterTypes();
-    // for (int i = 0; i < paramTypes.length; i++) {
-    // afficheTypeBasic(paramTypes[i], decalage);
-    // if (i < paramTypes.length - 1) {
-    // System.out.print(", ");
-    // }
-    // }
-    // System.out.println(");");
-    // }
+    Constructor<?>[] constructors = cl.getDeclaredConstructors();
+    for (Constructor<?> constructor : constructors) {
+      System.out.print(decalage + Modifier.toString(constructor.getModifiers()) + " " + constructor.getName() + "(");
+      Class<?>[] paramTypes = constructor.getParameterTypes();
+      Type[] genericParamTypes = constructor.getGenericParameterTypes();
+      afficheParamsFull(paramTypes, genericParamTypes, decalage);
+      System.out.println(");");
+    }
   }
 
   public static void afficheMethodes(Class<?> cl, String decalage) {
     // Affichage des méthodes de la classe : utilise afficheParamsBasic ou
     // afficheParamsFull
-    // Code à écrire
-    // Method[] methods = cl.getDeclaredMethods();
-    // for (Method method : methods) {
-    // System.out.print(decalage + Modifier.toString(method.getModifiers()) + " ");
-    // afficheTypeBasic(method.getReturnType(), decalage);
-    // System.out.print(" " + method.getName() + "(");
-    // Class<?>[] paramTypes = method.getParameterTypes();
-    // for (int i = 0; i < paramTypes.length; i++) {
-    // afficheTypeBasic(paramTypes[i], decalage);
-    // if (i < paramTypes.length - 1) {
-    // System.out.print(", ");
-    // }
-    // }
-    // System.out.println(");");
-    // }
-
+    Method[] methods = cl.getDeclaredMethods();
+    for (Method method : methods) {
+      System.out.print(decalage + Modifier.toString(method.getModifiers()) + " ");
+      afficheTypeBasic(method.getReturnType(), decalage);
+      System.out.print(" " + method.getName() + "(");
+      Class<?>[] paramTypes = method.getParameterTypes();
+      Type[] genericParamTypes = method.getGenericParameterTypes();
+      afficheParamsFull(paramTypes, genericParamTypes, decalage);
+      System.out.println(");");
+    }
   }
 
   private static void afficheParamsBasic(Class<?>[] params, String decalage) {
     // Affichage du type de chaque paramètre : Ne prend pas en compte
-    // l'instanciation de types génériques (variante simple)
-    // Code à écrire
-    // for (int i = 0; i < params.length; i++) {
-    // System.out.print(decalage + params[i].getSimpleName());
-    // if (i < params.length - 1) {
-    // System.out.print(", ");
-    // }
-    // }
+    // l'instanciation de types génériques
+    for (int i = 0; i < params.length; i++) {
+      System.out.print(params[i].getSimpleName());
+      if (i < params.length - 1) {
+        System.out.print(", ");
+      }
+    }
   }
 
   private static void afficheParamsFull(Class<?>[] params, Type[] gparams, String decalage) {
     // Affichage du type de chaque paramètre : Prend en compte l'instanciation de
-    // types génériques (variante plus complexe)
-    // Code à écrire
-    // for (int i = 0; i < params.length; i++) {
-    // afficheTypeFull(params[i], gparams[i], decalage);
-    // if (i < params.length - 1) {
-    // System.out.print(", ");
-    // }
-    // }
+    // types génériques
+    for (int i = 0; i < params.length; i++) {
+      afficheTypeFull(params[i], gparams[i], decalage);
+      if (i < params.length - 1) {
+        System.out.print(", ");
+      }
+    }
   }
 
   public static void main(String[] args) {
-    // Code à écrire : :
-    // Récupérer le nom de la classe dans 'args'
-    // Lancement de l'analyse de la classe
-    // Exemple d'appel : java App java.util.HashMap
-    String classe = "java.util.HashMap";
+    // Récupérer le nom de la classe dans 'args' avec valeur par défaut
+    String classe = (args.length >= 1 && args[0] != null && !args[0].isBlank())
+        ? args[0]
+        : "java.util.HashMap";
     try {
       analyseClasse(classe, "");
     } catch (ClassNotFoundException e) {
